91c590cf2b545277872a13e530895e7745f961c3
Mateus Junges <mateusf.junges@gmail.com>
2020-04-01 11:33:36 -0300

    Added two factor middleware

---
+++ src/Http/Middleware/TwoFactorAuthMiddlware.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class TwoFactorAuthMiddlware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = Auth::user();
+
+        if (Auth::check() and $user->two_factor_code) {
+            if ($user->two_factor_expires_at->lt(now())) {
+                $user->resetTwoFactorCode();
+                Auth::logout();
+                return redirect()
+                    ->route('login')
+                    ->withMessage('Your two factor code has been expired. Please, login again.');
+            }
+            if ($request->is('verify*')) {
+                return redirect()->route('verify.two_factor_code');
+            }
+        }
+        return $next($request);
+    }
+}
