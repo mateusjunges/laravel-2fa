46ce6371a3b0f24a23d810e294db1044710282c6
Mateus Junges <mateusf.junges@gmail.com>
2020-04-02 14:43:43 -0300

    Fix namespace

--- tests/Http/Middleware/TwoFactorAuthMiddlewareTest.php
+++ tests/Http/Middleware/TwoFactorAuthMiddlewareTest.php
@@ -2,17 +2,23 @@
 
 namespace Junges\TwoFactorAuth\Tests\Http\Middleware;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class TestTwoFactorAuthMiddleware extends MiddlewareTestCase
 {
-    public function test_it_deny_access_to_users_with_wrong_two_factor_code()
-    {
-    }
-
     public function test_it_allow_access_to_routes_not_protected_by_two_factor_middleware()
     {
+        $this->assertEquals(
+            $this->execMiddleware(
+                $this->twoFactorMiddleware,
+                ""
+            ),
+            Response::HTTP_UNAUTHORIZED
+        );
     }
 
     public function test_it_deny_access_to_not_authenticated_users()
     {
+
     }
 }
