01931fbfdf639fae871d7143eb8e4a38f75b9880
Mateus Junges <mateus@junges.dev>
2020-04-02 15:55:59 +0000

    Apply fixes from StyleCI

--- src/Events/TwoFactorCodeConfirmed.php
+++ src/Events/TwoFactorCodeConfirmed.php
@@ -8,7 +8,9 @@ use Illuminate\Queue\SerializesModels;
 
 class TwoFactorCodeConfirmed
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
 
     public $user;
 
