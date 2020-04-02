3b625882a3274e4ea25b5284a3b35e74ba3f7e59
Mateus Junges <mateusf.junges@gmail.com>
2020-04-02 14:45:32 -0300

    Redirect user to login if logged out

--- src/Http/Middleware/TwoFactorAuthMiddleware.php
+++ src/Http/Middleware/TwoFactorAuthMiddleware.php
@@ -17,25 +17,30 @@ class TwoFactorAuthMiddleware
      */
     public function handle($request, Closure $next)
     {
-        /** @var User $user */
-        $user = auth()->user();
+        if (auth()->check()) {
+            /** @var User $user */
+            $user = auth()->user();
 
-        if (auth()->check() && $user->two_factor_code) {
-            if ($user->getTwoFactorExpiration()->lt(now())) {
-                $user->resetTwoFactorCode();
+            if (auth()->check() && $user->two_factor_code) {
+                if ($user->getTwoFactorExpiration()->lt(now())) {
+                    $user->resetTwoFactorCode();
 
-                auth()->logout();
+                    auth()->logout();
 
-                return redirect()
-                    ->route('login')
-                    ->withMessage('Your two factor code has been expired. Please, login again.');
+                    return redirect()
+                        ->route('login')
+                        ->setStatusCode(401)
+                        ->withMessage('Your two factor code has been expired. Please, login again.');
+                }
+
+                if (!$request->is('two_factor_code*')) {
+                    return redirect()->route('two_factor_code.verify');
+                }
             }
+            return $next($request);
 
-            if (!$request->is('two_factor_code*')) {
-                return redirect()->route('two_factor_code.verify');
-            }
+        } else {
+            return redirect()->route('login');
         }
-
-        return $next($request);
     }
 }
