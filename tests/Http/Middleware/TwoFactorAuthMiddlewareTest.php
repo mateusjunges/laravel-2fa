10d2c9d4223df62d33f6086bbb61f09bf446e0b4
Mateus Junges <mateusf.junges@gmail.com>
2020-04-02 14:46:38 -0300

    Add tests for two factor auth middleware

--- tests/Http/Middleware/TwoFactorAuthMiddlewareTest.php
+++ tests/Http/Middleware/TwoFactorAuthMiddlewareTest.php
@@ -2,23 +2,48 @@
 
 namespace Junges\TwoFactorAuth\Tests\Http\Middleware;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
 
-class TestTwoFactorAuthMiddleware extends MiddlewareTestCase
+class TwoFactorAuthMiddlewareTest extends MiddlewareTestCase
 {
-    public function test_it_allow_access_to_routes_not_protected_by_two_factor_middleware()
+    public function test_it_does_not_allow_access_to_guest_users()
     {
         $this->assertEquals(
+            Response::HTTP_FOUND,
             $this->execMiddleware(
-                $this->twoFactorMiddleware,
-                ""
-            ),
-            Response::HTTP_UNAUTHORIZED
+                $this->twoFactorMiddleware
+            )
         );
     }
 
-    public function test_it_deny_access_to_not_authenticated_users()
+    public function test_if_logged_in_user_with_two_factor_confirmed_can_access_protected_routes()
     {
+        Auth::login($this->user);
+
+        $this->assertEquals(
+            Response::HTTP_OK,
+            $this->execMiddleware(
+                $this->twoFactorMiddleware
+            )
+        );
+
+    }
+
+    public function test_if_logged_in_user_without_two_factor_confirmed_can_not_access_protected_routes()
+    {
+        Auth::login($this->user);
+
+        $this->user->two_factor_code = Str::upper(Str::random(8));
+        $this->user->save();
+
+        $this->assertEquals(
+            Response::HTTP_UNAUTHORIZED,
+            $this->execMiddleware(
+                $this->twoFactorMiddleware
+            )
+        );
 
     }
 }
